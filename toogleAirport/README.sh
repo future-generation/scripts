f8cc23b8cff92c897361a61b4cb5ce73bcf1783e
future-generation <github@future-generation.dyndns.org>
2018-11-17 17:18:12 +0100

    Create README.sh

---
+++ toogleAirport/README.sh
@@ -0,0 +1,10 @@
+# toogleAirport
+Irgendwo gefunden
+## Script in folgendes Verzeichnis:
+/Library/Scripts/toggleAirport.sh
+## Rechte setzen:
+sudo chmod 755 /Library/Scripts/toggleAirport.sh
+## PLIST in folgendes Verzeichnis:
+/System/Library/LaunchAgents/com.mine.toggleairport.plist
+## Starten des Dienstes mit:
+sudo launchctl load /System/Library/LaunchAgents/com.mine.toggleairport.plist
