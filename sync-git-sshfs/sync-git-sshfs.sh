6348746cda253c030cc34318345ba6d542fae1c1
future-generation <github@future-generation.dyndns.org>
2019-03-13 18:04:28 +0100

    Update sync-git-sshfs.sh

--- sync-git-sshfs/sync-git-sshfs.sh
+++ sync-git-sshfs/sync-git-sshfs.sh
@@ -8,15 +8,14 @@ pass="[BASE64_ENCODED_PASSWORD]"
 sshkey=$HOME/.ssh/[SSH_KEY_FILE]
 host="[USERNAME]@[FQDN]"
 target="$HOME/[MOUNTFOLDER]"
-echo "$pass"|base64 --decode|sshfs -o password_stdin $host:/ $target	# Connect via sshfs with password
-#sshfs -o IdentityFile=$sshkey $host:/ $target	# Connect via sshfs with certificate
-cd $target	# Move into sshfs mountpoint
+echo "$pass"|base64 --decode|sshfs -o password_stdin $host:/ $target # Connect via sshfs with password
+#sshfs -o IdentityFile=$sshkey $host:/ $target # Connect via sshfs with certificate
+cd $target/ttrss # Move into sshfs mountpoint
 if ! [[ "$(git log --pretty=%H ...refs/heads/master^ | head -n 1)" = "$(git ls-remote origin -h refs/heads/master |cut -f1)" ]]; then
-	git pull origin master	# Update remote folder with git repo
-	terminal-notifier -title "Tiny Tiny RSS" -subtitle "Local branch synced" -message "Git pull finished"
+	git pull origin master # Update remote folder with git repo
+	terminal-notifier -title "Tiny Tiny RSS" -subtitle "Local branch synced" -message "Git pull finished" &>/dev/null
 else
-	echo "Nothing new in git repo. No sync needed."
-	terminal-notifier -title "Tiny Tiny RSS" -subtitle "Local branch up-to-date" -message "No git pull needed"
+	terminal-notifier -title "Tiny Tiny RSS" -subtitle "Local branch up-to-date" -message "No git pull needed" &>/dev/null
 fi
-cd $HOME	# Move out otherwise unmount sshfs target is blocked
-umount $target	# Unmount sshfs connection
+cd $HOME # Move out otherwise unmount sshfs target is blocked
+umount $target &>/dev/null # Unmount sshfs connection
